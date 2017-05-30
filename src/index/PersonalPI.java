195c38f13646d2cfe9757dca49dcd9f9300615ea
Elizabeth Cho <zergling13rush@gmail.com>
2017-05-30 10:03:31 -0400

    So many things added
    someone help

--- src/index/PersonalPI.java
+++ src/index/PersonalPI.java
@@ -9,10 +9,16 @@ public class PersonalPI {
 	public static String[] emp = {"", "Full time", "Part time",
 			"Not employed", "Retired", "Self employed", "Military",
 			"Other"};
+	public static String[] covType = {"", "Primary", "Secondary",
+			"Tertiary"};
+	public static String[] rel = {"", "Self", "Spouse", "Child",
+			"Father", "Mother", "Siblings"};
+	public static String[] gender = {"", "Male", "Female"};
 	
 	public static JComboBox raceC = new JComboBox(race);
 	public static JComboBox empC = new JComboBox(emp);
-	
+	public static JComboBox covTypeC = new JComboBox(covType);
+	public static JComboBox relC = new JComboBox(rel);
 	
 	public static JTextField fN = new JTextField();
 	public static JTextField mN = new JTextField();
@@ -37,6 +43,10 @@ public class PersonalPI {
 	public static JLabel eNumL = new JLabel("Phone number of emergency contact");
 	public static JLabel insProvL = new JLabel("Insurance Provider");
 	public static JLabel insIDL = new JLabel("Member ID");
+	public static JLabel raceL = new JLabel("Race");
+	public static JLabel empL = new JLabel("Employer");
+	public static JLabel covTypeL = new JLabel("Type of Coverage");
+	public static JLabel relL = new JLabel("Relation to holder");
 	
 	public static JLabel nameA = new JLabel();
 	public static JLabel nameB = new JLabel();
@@ -46,11 +56,13 @@ public class PersonalPI {
 	public static JLabel conD = new JLabel();
 	public static JLabel conE = new JLabel();
 	public static JLabel conF = new JLabel();
+	public static JLabel raceA = new JLabel();
 	
 	public static JPanel nameP = new JPanel();		//3x3
 	public static JPanel addrP = new JPanel();		//2x1
 	public static JPanel contactP = new JPanel();	//2x2
 	public static JPanel birthP = new JPanel();		//2x4?
 	public static JPanel eConP = new JPanel();		//2x2
-	public static JPanel insP = new JPanel();		//something
+	public static JPanel insInputP = new JPanel();	//2x2
+	public static JPanel insComboP = new JPanel();	//2x2
 }
